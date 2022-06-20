79d9698dafb3105126f122068f05b49d79814efd
hamma <mohamedhamma445@gmail.com>
2022-06-20 12:43:00 +0200

    initialisation

---
+++ src/main/java/resources/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.hamma.resources;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hamma.models.HelloWorld;
+import com.hamma.services.ExempleServices;
+
+@Controller
+public class HelloWorldController {
+	@Autowired
+	private ExempleServices exempleServices;
+
+    @RequestMapping("/helloworld")
+    public String handler(Model model) {
+
+        HelloWorld helloWorld = exempleServices.traiter();
+        model.addAttribute("helloWorld", helloWorld);
+        return "helloworld";
+    }
+}
